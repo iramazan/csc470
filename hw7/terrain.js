b5ebe530156af52d17b5999bff38f8ef113c2854
iramazan <iramazan96@gmail.com>
2017-12-01 21:23:02 -0500

    Homework 7: Perlin Noise Implementation

---
+++ hw7/terrain.js
@@ -0,0 +1,54 @@
+var canvas;
+var gl;
+var shader_program;
+
+window.onload = function main()
+{
+    gl_init();
+    compile_shaders();
+};
+
+/*
+ * Initialize gl context
+ */
+function gl_init()
+{
+    canvas = document.getElementById("gl_canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0, 0, 0, 0);
+}
+
+/*
+ * Compile Shaders
+ */
+function compile_shaders()
+{
+    // vertex shader
+    var vert_shader = gl.createShader(gl.VERTEX_SHADER);
+    var vert_code = document.getElementById("vertex_shader");
+    gl.shaderSource(vert_shader, vert_code.text);
+    gl.compileShader(vert_shader);
+    if (!gl.getShaderParameter(vert_shader, gl.COMPILE_STATUS)) {
+        throw new Error(gl.getShaderInfoLog(vert_shader));
+    }
+
+    // fragment shader
+    var frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
+    var frag_code = document.getElementById("fragment_shader");
+    gl.shaderSource(frag_shader, frag_code.text);
+    gl.compileShader(frag_shader);
+    if (!gl.getShaderParameter(frag_shader, gl.COMPILE_STATUS)) {
+        throw new Error(gl.getShaderInfoLog(frag_shader));
+    }
+
+    // create shader program
+    shader_program = gl.createProgram();
+    gl.attachShader(shader_program, vert_shader);
+    gl.attachShader(shader_program, frag_shader);
+    gl.linkProgram(shader_program);
+    if (!gl.getProgramParameter(shader_program, gl.LINK_STATUS)) {
+        throw new Error(gl.getProgramInfoLog(shader_program));
+    }
+    gl.useProgram(shader_program);
+}
