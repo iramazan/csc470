5af8a76b1b50043bb36eac13830839a935947aa2
iramazan <iramazan96@gmail.com>
2017-12-03 15:06:35 -0500

    Generate 2d height map array using multiple octaves of noise.

--- hw7/terrain.js
+++ hw7/terrain.js
@@ -1,6 +1,7 @@
 var canvas;
 var gl;
 var shader_program;
+var height_map = [];
 
 window.onload = function main()
 {
@@ -8,6 +9,15 @@ window.onload = function main()
     compile_shaders();
 };
 
+/*
+ * Transoform t in interval [a,b] to [c,d]
+ * f(t) = c + ((d - c) / (b - a)) * (t - a)
+ */
+function intv(t, a, b, c, d)
+{
+    return c + ((d - c) / (b - a)) * (t - a);
+}
+ 
 /*
  * Initialize gl context
  */
@@ -51,4 +61,24 @@ function compile_shaders()
         throw new Error(gl.getProgramInfoLog(shader_program));
     }
     gl.useProgram(shader_program);
+}
+
+/*
+ * Generate a height map
+ */
+function height_map()
+{
+    var width = 512;
+    var height = 512;
+    // generation loop
+    for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+            var nx = x / width - 0.5;
+            var ny = y / height - 0.5;
+            var e = 1 * simplex_noise(1 * nx,  1 * ny) +
+                0.5 * simplex_noise(2 * nx, 2 * ny) +
+                0.25 * simplex_noise(4 * nx, 2 * ny);
+            height_map[y][x] = Math.pow(e, 1.51);
+        }
+    }
 }
