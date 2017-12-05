0e98fec9e141ff4230ab65a7c299542e8b44366b
iramazan <iramazan96@gmail.com>
2017-12-05 14:36:26 -0500

    Wrote render and height assignment functions.

--- hw7/terrain.js
+++ hw7/terrain.js
@@ -2,11 +2,17 @@ var canvas;
 var gl;
 var shader_program;
 var height_map = [];
+var vertices = [];
+var scene_width = 128;
+var scene_height = 128;
 
 window.onload = function main()
 {
     gl_init();
     compile_shaders();
+    gen_height_map();
+    vert_height();
+    render();
 };
 
 /*
@@ -26,7 +32,7 @@ function gl_init()
     canvas = document.getElementById("gl_canvas");
     gl = WebGLUtils.setupWebGL(canvas);
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0, 0, 0, 0);
+    gl.clearColor(0, 0, 0, 1);
 }
 
 /*
@@ -66,19 +72,49 @@ function compile_shaders()
 /*
  * Generate a height map
  */
-function height_map()
+function gen_height_map()
 {
-    var width = 512;
-    var height = 512;
     // generation loop
-    for (var y = 0; y < height; y++) {
-        for (var x = 0; x < width; x++) {
-            var nx = x / width - 0.5;
-            var ny = y / height - 0.5;
-            var e = 1 * simplex_noise(1 * nx,  1 * ny) +
-                0.5 * simplex_noise(2 * nx, 2 * ny) +
-                0.25 * simplex_noise(4 * nx, 2 * ny);
-            height_map[y][x] = Math.pow(e, 1.51);
+    for (var x = 0; x < scene_width; x++) {
+        var column = [];
+        for (var y = 0; y < scene_height; y++) {
+            var nx = x / scene_width - 0.5;
+            var ny = y / scene_height - 0.5;
+            var e = 1 * intv(simplex_noise(1 * nx,  1 * ny), -1, 1, 0, 1) +
+                0.5 * intv(simplex_noise(2 * nx, 2 * ny), -1, 1, 0, 1) +
+                0.25 * intv(simplex_noise(4 * nx, 2 * ny), -1, 1, 0, 1);
+            column.push(Math.pow(e, 1.51));
+        }
+        height_map.push(column);
+    }
+}
+
+/*
+ * Create vertices based on height map
+ */
+function vert_height()
+{
+    for (var i = 0; i < height_map.length; i++) {
+        var dim = height_map[i];
+        for (var j = 0; j < dim.length; j++) {
+            vertices.push([i-scene_width/2, dim[j], j-scene_height/2, 1]);
         }
     }
+}
+
+/*
+ * Render the scene
+ */
+function render()
+{
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // buffer vertices
+    var vert_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vert_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    var vert_coord = gl.getAttribLocation(shader_program, "coordinate");
+    gl.vertexAttribPointer(vert_coord, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vert_coord);
+    // draw
+    gl.drawArrays(gl.TRIANGLE, 0, vertices.length/4);
 }
